1d067adb5e67bc055e6807edc476e5a319b1c85a
kiyo-astro <kiyoaki@uw.edu>
2026-02-25 15:39:13 -0800

    Add parse_summary2table.

--- heavens_above.py
+++ heavens_above.py
@@ -18,6 +18,12 @@ import requests
 import re
 import html
 import numpy as np
+import math
+from urllib.parse import urlparse, parse_qs
+
+from astropy.time import Time, TimeDelta
+from astropy.table import Table
+import astropy.units as u
 
 #--------------------------------------------------------------------------------------------------#
 # Main                                                                                             #
@@ -100,6 +106,177 @@ def parse_summary2mjd(
     mjds = np.array(list(dict.fromkeys(mjds)))
 
     return mjds
+
+def parse_summary2table(
+        query_result: str,
+        satname: str = ""
+        ):
+    """
+    Parse satellite pass summary to pass MJD list
+
+    Parameters
+    ----------
+    query_result: `str`
+        Satellite pass summary (HTML format)
+
+    Returns
+    -------
+    pass_table: `astropy.table.table.Table`
+        Satellite pass MJD list
+    satname: `str`
+        Satellite name. Default is "".
+
+    Notes
+    -----
+        (c) 2026 Kiyoaki Okudaira - University of Washington / IAU CPS SatHub
+    """    
+    # internal functions
+    def _strip_tags(s: str) -> str:
+        return re.sub(r"<.*?>", "", s).strip()
+
+
+    def _strip_deg(s: str) -> float:
+        return float(s.replace("°", "").strip())
+
+
+    def _extract_passdetails_url(tr_html: str) -> str:
+        # 1) <a href="...">
+        m = re.search(r'<a[^>]+href="([^"]*passdetails\.aspx[^"]*)"', tr_html)
+        if m:
+            return html.unescape(m.group(1))
+
+        # 2) onclick="window.location='...'"
+        m = re.search(r"window\.location\s*=\s*'([^']*passdetails\.aspx[^']*)'", tr_html)
+        if m:
+            return html.unescape(m.group(1))
+
+        return ""
+
+    def _qs_get_first(qs, key, default=None):
+        v = qs.get(key)
+        if not v:
+            return default
+        return v[0]
+    tr_list = re.findall(
+        r'<tr\s+class="clickableRow".*?</tr>',
+        query_result,
+        flags=re.DOTALL
+    )
+
+    # output canvas
+    cols = {
+        "satid": [],
+        "satname": [],
+        "mag": [],
+        "duration": [],
+        "start_utc": [],
+        "start_alt": [],
+        "start_az": [],
+        "max_utc": [],
+        "max_alt": [],
+        "max_az": [],
+        "end_utc": [],
+        "end_alt": [],
+        "end_az": [],
+        "visible": [],
+        "detail_url": [],
+        "mjd": []
+    }
+
+    for tr_html in tr_list:
+
+        url = _extract_passdetails_url(tr_html)
+
+        tds = re.findall(r"<td[^>]*>(.*?)</td>", tr_html, flags=re.DOTALL)
+        tds_text = [_strip_tags(x) for x in tds]
+
+        if len(tds_text) < 12:
+            continue
+
+        date_str = tds_text[0]
+        try:
+            brightness = float(tds_text[1])
+        except:
+            brightness = None
+
+        start_time = tds_text[2]
+        start_alt = _strip_deg(tds_text[3])
+        start_az = tds_text[4]
+
+        max_time = tds_text[5]
+        max_alt = _strip_deg(tds_text[6])
+        max_az = tds_text[7]
+
+        end_time = tds_text[8]
+        end_alt = _strip_deg(tds_text[9])
+        end_az = tds_text[10]
+
+        pass_type = tds_text[11]
+        if pass_type == "visible":
+            pass_type = True
+        else:
+            pass_type = False
+
+        # PassDetail query
+        qs = parse_qs(urlparse(url).query) if url else {}
+
+        satid = _qs_get_first(qs, "satid", None)
+        mjd = _qs_get_first(qs, "mjd", None)
+        # pass_kind = _qs_get_first(qs, "type", "")
+
+        satid_i = int(satid) if satid is not None else -1
+        mjd_f = float(mjd) if mjd is not None else float("nan")
+
+        max_time_obj = Time(mjd_f, format="mjd", scale="utc")
+        max_time = max_time_obj.isot[:11] + max_time
+        max_time_obj = Time(max_time)
+
+        start_time = max_time_obj.isot[:11] + start_time
+        start_time_obj = Time(start_time)
+        end_time = max_time_obj.isot[:11] + end_time
+        end_time_obj = Time(end_time)
+
+        duration = (end_time_obj - start_time_obj).sec
+        # duration_min = int(duration // 60)
+        # duration_sec = int(duration % 60)
+
+        # duration_f = f"{duration_min:02d}:{duration_sec:02d}"
+
+        if start_time_obj > max_time_obj:
+            start_time_obj - TimeDelta(1 * u.day)
+        if end_time_obj < max_time_obj:
+            end_time_obj + TimeDelta(1 * u.day)
+
+        start_time = start_time_obj.isot[0:19]
+        max_time = max_time_obj.isot[0:19]
+        end_time = end_time_obj.isot[0:19]
+
+        # output
+        # cols["date_str"].append(date_str)
+        cols["satid"].append(satid_i)
+        cols["satname"].append(satname)
+        cols["mag"].append(brightness)
+        cols["duration"].append(int(duration))
+        cols["start_utc"].append(start_time)
+        cols["start_alt"].append(start_alt)
+        cols["start_az"].append(start_az)
+        cols["max_utc"].append(max_time)
+        cols["max_alt"].append(max_alt)
+        cols["max_az"].append(max_az)
+        cols["end_utc"].append(end_time)
+        cols["end_alt"].append(end_alt)
+        cols["end_az"].append(end_az)
+        cols["visible"].append(pass_type)
+        cols["detail_url"].append(url)
+        cols["mjd"].append(mjd_f)
+        # cols["pass_kind"].append(pass_kind)
+    
+    pass_table = Table(
+        cols,
+        dtype=[object, object, object, object, object, object, object, object, object, object, object, object, object, object, object, object]
+        )
+
+    return pass_table
 
 def get_pass_detail(
         norad_id: int | str,
